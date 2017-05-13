cb1d21022b432a36b4dadc2d99fc24581c3b9e91
oleno <pirit.lena@gmail.com>
2017-05-13 19:31:43 +0300

    02. Jigsaw Puzzle. Starting the game

--- 02_Jigsaw-Puzzle-Game/js/puzzle.js
+++ 02_Jigsaw-Puzzle-Game/js/puzzle.js
@@ -4,6 +4,33 @@ var cols = 4;
 $(document).ready(function() {
 	var sliceStr = createSlices(true);
 	$('#puzzleContainer').html(sliceStr);
+
+	// Starting the game
+	$('#start').on('click', function()
+	{
+		var divs = $('#puzzleContainer > div');
+		var allDivs = shuffle(divs);
+		$('pieceBox').empty();
+		allDivs.each(function() {
+			var leftDistance = Math.floor((Math.random()*280)) + 'px';
+			var topDistance = Math.floor((Math.random()*280)) + 'px';
+
+			$(this)
+				.addClass('imgDroppable')
+				.css({
+					position : 'absolute',
+					left : leftDistance,
+					top : topDistance
+				});
+				$('#pieceBox').append($(this));
+		});
+
+		var sliceStr = createSlices(false);
+		$('#puzzleContainer').html(sliceStr);
+
+		$(this).hide();
+		$('#reset').show();
+	});
 });
 
 function createSlices(useImage)
@@ -16,14 +43,21 @@ function createSlices(useImage)
 		{
 			if(useImage)
 			{
-				sliceArr.push('<div style="background-position: ' + left + 'px ' + top +'px;" class="img" data-sequence="'+ c + '">');
+				str+= '<div style="background-position: ' + left + 'px ' + top +'px;" class="img" data-sequence="'+ c + '">';
 			}
 			else 
 			{
-				sliceArr.push('<div style="background-image: none;" class="img imgDroppable">');
+				str+='<div style="background-image: none;" class="img imgDroppable">';
 			}
-			sliceArr.push('</div>');
+			str+= '</div>';
 		}
 	}
-	return sliceArr.join('');
+	return str;
 }
+
+function shuffle(o) 
+{
+	for (var j, x, i = o.length; i; 
+			j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
