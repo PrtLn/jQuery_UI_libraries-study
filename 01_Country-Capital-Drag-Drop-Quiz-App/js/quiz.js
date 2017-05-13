e9b028d64a4fc810dd560891fa699c693ec2cf04
oleno <pirit.lena@gmail.com>
2017-05-13 17:27:31 +0300

    01. Quiz App. Displaying data on the page

---
+++ 01_Country-Capital-Drag-Drop-Quiz-App/js/quiz.js
@@ -0,0 +1,34 @@
+$(document).ready(function()
+{
+	createQuizLayout();
+});
+
+function createQuizLayout()
+{
+	var countries = ["USA", "Uk", "India", "Germany", "Turkey", 
+					"France", "Nepal", "Japan", "South Africa", "Maldives"];
+	var capitals = ["Washington", "London", "Delhi", "Berlin", "Istanbul",
+					"Paris", "Kathmandu", "Tokio", "Capetown", "Male"];
+
+	var arrCountry = [];
+	for (var i = 0; i < countries.length; i++) {
+		arrCountry.push('<li data-index="' + (i+1) + '"> ' + countries[i] + '</li>');
+	}
+	var arrCapital = [];
+	for (var i = 0; i < capitals.length; i++) {
+		arrCapital.push('<li data-index="' + (i+1) + '"> ' + capitals[i] + '</li>');
+	}
+
+	arrCountry = shuffle(arrCountry);
+	arrCapital = shuffle(arrCapital);
+
+	$('#source').html(arrCountry.join(''));
+	$('#target').html(arrCapital.join(''));
+}
+
+function shuffle(o) 
+{
+	for (var j, x, i = o.length; i; 
+			j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
